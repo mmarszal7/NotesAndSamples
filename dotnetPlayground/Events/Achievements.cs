ae7fcd62b1c2c851fecf9534ad19baddb3da173c
mmarszalek <mmarszalek@amctech.pl>
2017-09-15 10:49:50 +0200

    Events sample

---
+++ dotnetPlayground/Events/Achievements.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Events
+{
+    public class Achievements
+    {
+        public Achievements()
+        {
+            // 3.2. Subskrypcja delegata w konstruktorze eventu
+            Game.OnWin += () => Console.WriteLine("You won!");
+        }
+    }
+}
